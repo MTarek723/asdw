f36a7915b1d07a95d10d9c5f434be3b11c8fb302
Mahmoud Tarek <138780850+MTarek723@users.noreply.github.com>
2023-10-17 20:45:29 +0300

    Create excut.c

---
+++ excut.c
@@ -0,0 +1,31 @@
+#include "main.h"
+void excut(char **argv)
+{
+int stats;
+char *command;
+char *real_command;
+pid_t pid;
+if (argv)
+{
+command = argv[0];
+real_command = locate(command);
+pid = fork();
+
+if (pid == 0)
+{
+if (execve(real_command, argv, 0) == -1)
+{
+perror(argv[0]);
+}
+exit(EXIT_FAILURE);
+}
+else if (pid < 0)
+{
+perror("Error:");
+}
+else
+{
+waitpid(pid, &stats, 0);
+}
+}
+}
